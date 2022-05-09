92a7dd9b7bcd042507c87d10918b8e112b910202
Joseph Adrian Pelgone <pelgonejoseph@gmail.com>
2022-05-09 13:13:10 +0800

    package installed

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom'
 import { Button } from 'react-bootstrap';
 
 function Index(){
-	return (
-		<div>
-		</div>
-	)
+  return (
+    <div>
+    </div>
+  )
 }
 
 export default Index;
